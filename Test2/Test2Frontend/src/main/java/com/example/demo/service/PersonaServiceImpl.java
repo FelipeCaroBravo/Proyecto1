a62fdda2707a32d41aa461b1b2ca49168e5b6e65
FelipeCaroBravo <felipecarobravo1@gmail.com>
2026-04-01 22:23:08 -0300

    Update PersonaServiceImpl.java

--- Test2/Test2Frontend/src/main/java/com/example/demo/service/PersonaServiceImpl.java
+++ Test2/Test2Frontend/src/main/java/com/example/demo/service/PersonaServiceImpl.java
@@ -1,113 +1,38 @@
 package com.example.demo.service;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.entity.PersonaEntity;
 import com.example.demo.interfeces.IPersonaService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
-public class PersonaServiceImpl implements IPersonaService{
+public class PersonaServiceImpl implements IPersonaService {
 
-	@Override
-	public List<PersonaEntity> listarTodos() {
-	    try {
-	        ObjectMapper unMapper = new ObjectMapper();
-	        List<PersonaEntity> personas = Arrays.asList(unMapper
-	            .readValue(new URL("http://localhost:8886/api/v1/entities/persona/"),
-	                PersonaEntity[].class)
-	        );
-	        return personas;
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
+    private final String URL = "http://spring-app-1:8886/api/v1/entities/persona";
+    private final RestTemplate restTemplate = new RestTemplate();
 
-	@Override
-	public PersonaEntity findById(long id) {
+    @Override
+    public List<PersonaEntity> listarTodos() {
+        PersonaEntity[] personas = restTemplate.getForObject(URL + "/", PersonaEntity[].class);
+        return Arrays.asList(personas);
+    }
 
-	    RestTemplate restTemplate = new RestTemplate();
+    @Override
+    public PersonaEntity findById(long id) {
+        return restTemplate.getForObject(URL + "/" + id, PersonaEntity.class);
+    }
 
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setAccept(Collections.singletonList(
-	            MediaType.APPLICATION_JSON));
+    @Override
+    public PersonaEntity save(PersonaEntity persona) {
+        return restTemplate.postForObject(URL + "/", persona, PersonaEntity.class);
+    }
 
-	    HttpEntity<PersonaEntity> entity =
-	            new HttpEntity<PersonaEntity>(httpHeaders);
-
-	    try {
-	        ResponseEntity<PersonaEntity> response = restTemplate.exchange(
-	                "http://localhost:8886/api/v1/entities/persona/" + id,
-	                HttpMethod.GET,
-	                entity,
-	                PersonaEntity.class);
-
-	        return response.getBody();
-
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-
-	@Override
-	public PersonaEntity save(PersonaEntity persona) {
-
-	    RestTemplate restTemplate = new RestTemplate();
-
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-	    httpHeaders.setAccept(Collections.singletonList(
-	            MediaType.APPLICATION_JSON));
-
-	    HttpEntity<PersonaEntity> entity =
-	            new HttpEntity<>(persona, httpHeaders);
-
-	    try {
-	        ResponseEntity<PersonaEntity> response = restTemplate.exchange(
-	                "http://localhost:8886/api/v1/entities/persona/",
-	                HttpMethod.POST,
-	                entity,
-	                PersonaEntity.class);
-
-	        return response.getBody();
-
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}
-
-	@Override
-	public void deleteById(long id) {
-
-	    RestTemplate restTemplate = new RestTemplate();
-
-	    HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setAccept(Collections.singletonList(
-	            MediaType.APPLICATION_JSON));
-
-	    HttpEntity<PersonaEntity> entity
-	            = new HttpEntity<PersonaEntity>(httpHeaders);
-
-	    ResponseEntity<PersonaEntity> pe = restTemplate.exchange(
-	            "http://localhost:8886/api/v1/entities/persona/" + id,
-	            HttpMethod.DELETE,
-	            entity,
-	            PersonaEntity.class);
-	}
-
+    @Override
+    public void deleteById(long id) {
+        restTemplate.delete(URL + "/" + id);
+    }
 }
