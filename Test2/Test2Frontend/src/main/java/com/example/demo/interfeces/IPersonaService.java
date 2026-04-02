b69f8e6fd3731df82b039937c4d653ed3e539231
FelipeCaroBravo <felipecarobravo1@gmail.com>
2026-04-01 22:37:47 -0300

    Update IPersonaService.java

--- Test2/Test2Frontend/src/main/java/com/example/demo/interfeces/IPersonaService.java
+++ Test2/Test2Frontend/src/main/java/com/example/demo/interfeces/IPersonaService.java
@@ -1,12 +1,17 @@
 package com.example.demo.interfeces;
 
 import java.util.List;
-
 import com.example.demo.entity.PersonaEntity;
 
-public interface IPersonaService{
-	public List<PersonaEntity> listarTodos();
-	PersonaEntity findById(long Id);
-	PersonaEntity save(PersonaEntity persona);
-	void deleteById(long Id);
+public interface IPersonaService {
+
+    List<PersonaEntity> listarTodos();
+
+    PersonaEntity findById(long id);
+
+    PersonaEntity save(PersonaEntity persona);
+
+    PersonaEntity update(long id, PersonaEntity persona);
+
+    void deleteById(long id);
 }
